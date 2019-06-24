1ba90d6c63568ffb4c23c1209d66b7220880a3ea
Emilio Amaya <emilio.a.amaya@vanderbilt.edu>
2019-06-24 11:19:06 -0500

    marker functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello from JavaScript');
+const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW1heWFlYSIsImEiOiJjanhhazA1cjgxNzdwM29xYnhqcW9uMjltIn0.nEQUlsoYThdiSPeH-34GYQ';
+
+const map = new mapboxgl.Map({
+    container: "map",
+    center: [-87.6354, 41.8885], 
+    zoom: 12, // starting zoom
+    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available
+  });
+
+const marker = buildMarker("activities", [-87.6354, 41.8885]);
+marker.addTo(map);
